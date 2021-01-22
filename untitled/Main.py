d0af27d1927f42d5a1da1e7d37bd9ad2a389360d
Sokitbayev Meirzhan <46749286+Sokitbayev@users.noreply.github.com>
2021-01-22 17:20:39 +0600

    Update Main.py

--- untitled/Main.py
+++ untitled/Main.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot('1095476545:AAHn5EOEn6Xyen3gZ28w5sDmJnvs_WNe4k0')
 @bot.message_handler(commands=['start'])
 def start(message):
     while(True):
-        bot.send_message(message.from_user.id, 'Shamsh Pidr!!!')
+        bot.send_message(message.from_user.id, 'Hello')
 
 
 bot.polling()
